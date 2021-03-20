06438bf790f7a2b627819a80403565bccdd55235
Onyemaechi Okafor <peerwaya@yahoo.com>
2021-03-20 18:28:06 +0100

    Update FlutterRTCPixelBufferConverter.m

--- macos/Classes/FlutterRTCPixelBufferConverter.m
+++ macos/Classes/FlutterRTCPixelBufferConverter.m
@@ -1,1 +1,1 @@
-/Users/evenmatrix/Desktop/project/gotok_w/gotok_libs/flutter-webrtc/common/darwin/Classes/FlutterRTCPixelBufferConverter.m
+../../common/darwin/Classes/FlutterRTCPixelBufferConverter.m
